c63003a43a4d4deb05b6ed792a08a66d16acd276
Nazma1997 <nazma.ist.707@gmail.com>
2022-04-20 23:27:32 -0800

    added

--- L-6(Array-Object)/reduce.js
+++ L-6(Array-Object)/reduce.js
@@ -1,4 +1,4 @@
-const numbers = [1, 2, 3, 5, ' ', false, NaN, 4, 6];
+const numbers = [1, 2, 3, 5, '', false, NaN, 4, 6];
 
 // convert Array by reducers 
 
@@ -17,4 +17,39 @@ const result = numbers.reduce((acc, cur, index) => {
 
 console.log(result)
 
+// Convert array 
 
+const result2 = numbers.reduce((acc, cur ) => {
+    if (cur){
+        acc.push(cur.toString());
+    }
+
+    return acc;
+}, [])
+
+console.log(result2);
+
+
+// Own Reduce method 
+
+function myReduce(array, callBack, init){
+    let acc = init;
+    for(let i = 0; i < array.length; i++){
+        acc = callBack(acc, array[i], i, array);
+    }
+
+    return acc;
+}
+
+const result3 = myReduce(numbers, (acc, cur) => {
+
+          if(cur){
+              acc.push(cur *cur);
+          }
+
+          return acc;
+          
+},[]);
+
+
+console.log('Result3 :', result3)
