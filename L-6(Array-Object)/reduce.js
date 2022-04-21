f116ffc489f8786d061558656963bd19fca9c761
Nazma1997 <nazma.ist.707@gmail.com>
2022-04-20 22:29:03 -0800

    added

--- L-6(Array-Object)/reduce.js
+++ L-6(Array-Object)/reduce.js
@@ -1,11 +1,12 @@
 const numbers = [1, 2, 3, 5, ' ', false, NaN, 4, 6];
 
+// convert Array by reducers 
 
 const result = numbers.reduce((acc, cur, index) => {
     
     if (index == 0) acc += '[';
     if (cur) {
-        acc += cur.toString() + (index < numbers.length - 1) ? ',' : '';
+        acc += cur.toString() + (index < numbers.length - 1 ? ',' : '');
     }
     if(index == numbers.length - 1 ) acc += ']';
     
@@ -15,3 +16,5 @@ const result = numbers.reduce((acc, cur, index) => {
 
 
 console.log(result)
+
+
